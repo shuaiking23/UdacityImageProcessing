f3fb18bf54d18ba4060b500c24b6b9e72ad481b7
Francis <francis@treshna.com>
2023-02-19 17:01:55 +0800

    added typescript-eslint/parser

--- src/tests/helpers/reporter.ts
+++ src/tests/helpers/reporter.ts
@@ -1,17 +1,23 @@
-import {DisplayProcessor, SpecReporter, StacktraceOption} from "jasmine-spec-reporter";
+import {
+  DisplayProcessor,
+  SpecReporter,
+  StacktraceOption,
+} from 'jasmine-spec-reporter';
 import CustomReporter = jasmine.CustomReporter;
 import SuiteInfo = jasmine.JasmineStartedInfo;
 
 class CustomProcessor extends DisplayProcessor {
-    public displayJasmineStarted(info: SuiteInfo, log: string): string {
-        return `${log}`;
-    }
+  public displayJasmineStarted(info: SuiteInfo, log: string): string {
+    return `${log}`;
+  }
 }
 
 jasmine.getEnv().clearReporters();
-jasmine.getEnv().addReporter(new SpecReporter({
+jasmine.getEnv().addReporter(
+  new SpecReporter({
     spec: {
-        displayStacktrace: StacktraceOption.NONE
+      displayStacktrace: StacktraceOption.NONE,
     },
     customProcessors: [CustomProcessor],
-}));
+  })
+);
