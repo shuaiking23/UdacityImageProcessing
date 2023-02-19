ff79a820d34aa0dd163c60d65df1a400b0cfdd76
Francis <francis@treshna.com>
2023-02-19 18:16:00 +0800

    added handling for page not found

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,10 @@ const port = 3000;
 
 app.use('/api', routes);
 
+app.get('*', function(req, res){
+  res.status(404).send('Page Not Found!');
+});
+
 app.listen(port, () => {
   console.log(`Server started at localhost:${port}`);
 });
