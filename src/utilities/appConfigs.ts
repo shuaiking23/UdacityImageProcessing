67bdc43ec368801633ed9e69024440a2c90dc40a
Francis <francis@treshna.com>
2023-02-20 09:52:21 +0800

    added appConfigs

---
+++ src/utilities/appConfigs.ts
@@ -0,0 +1,8 @@
+export const STATIC_URL_PART = '/assets';
+export const IMAGES_URL_PART = '/images/';
+export const THUMBS_URL_PART = '/images/thumbs/';
+
+export const HOSTNAME = 'localhost';
+export const PORT = '3000';
+export const ASSET_PATH = process.cwd() + STATIC_URL_PART;
+
