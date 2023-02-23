296b1951af5c6aa227716c0db32e6e517d2b0490
Francis <francis@treshna.com>
2023-02-24 01:57:18 +0800

    added typing to variables (in test files)

--- src/tests/processImageSpec.ts
+++ src/tests/processImageSpec.ts
@@ -10,39 +10,39 @@ const {
 describe('3. Functionality tests', () => {
   describe('3.1 fileExists', () => {
     it('3.1.1 Not found (images)', () => {
-      const result = fileExists('name');
+      const result: boolean = fileExists('name');
       expect(result).toBe(false);
     });
 
     it('3.1.2 Not found (thumbs)', () => {
-      const result = fileExists('name', true);
+      const result: boolean = fileExists('name', true);
       expect(result).toBe(false);
     });
 
     it('3.1.3 found (images)', () => {
-      const result = fileExists('fjord');
+      const result: boolean = fileExists('fjord');
       expect(result).toBe(true);
     });
   });
 
   describe('3.2 resizeImage', () => {
-    const fileName = 'fjord';
-    const height = 200;
-    const width = 200;
-    const thumbFileName = `${fileName}_thumb_${height.toString()}_${width.toString()}`;
-    const outputImage = path.resolve(
+    const fileName: string = 'fjord';
+    const height: string = 200;
+    const width: string = 200;
+    const thumbFileName: string = `${fileName}_thumb_${height.toString()}_${width.toString()}`;
+    const outputImage: string = path.resolve(
       cfg.ASSET_PATH + cfg.THUMBS_URL_PART + thumbFileName + cfg.FILE_EXT
     );
 
     it('3.2.1 Test resize', async () => {
-      const result = await resizeImage(fileName, height, width);
+      const result: string = await resizeImage(fileName, height, width);
       expect(result).toEqual(outputImage);
-      const result2 = fileExists(thumbFileName, true);
+      const result2: boolean = fileExists(thumbFileName, true);
       expect(result2).toBe(true);
     });
     it('3.2.2 Remove Thumb after test resize', async () => {
       removeThumb(thumbFileName);
-      const result = fileExists(thumbFileName, true);
+      const result: boolean = fileExists(thumbFileName, true);
       expect(result).toBe(false);
     });
   });
